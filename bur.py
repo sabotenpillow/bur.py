0b6ac8042ec604c6835e11fe0474669a3aaad850
sabotenpillow <saboten.pillow@gmail.com>
2018-02-21 10:53:05 +0000

    work nfq process as thread

--- bur.py
+++ bur.py
@@ -1,8 +1,19 @@
 import os
 import sys
 import subprocess
+import threading
 from mynfq import MyNfq
 # from IPython import embed
+
+def nfqthread(queue_id):
+    nfq = MyNfq(queue_id)
+
+    try:
+        nfq.nfqueue.run()
+    except:
+        print("Exit.")
+
+    nfq.nfqueue.unbind()
 
 #if __name__ is '__main__':
 def main():
@@ -15,14 +26,10 @@ def main():
         'iptables -t raw -A OUTPUT -j NFQUEUE --queue-num '
         +str(QUEUE_ID)+' -s 192.168.67.10').split(' '))
 
-    nfq = MyNfq(QUEUE_ID)
+    nfqthr = threading.Thread(target=nfqthread, args=(QUEUE_ID,))
+    nfqthr.start()
+    nfqthr.join()
 
-    try:
-        nfq.nfqueue.run()
-    except:
-        print("Exit.")
-
-    nfq.nfqueue.unbind()
     subprocess.call('iptables -t raw -F'.split(' '))
 
 main()
