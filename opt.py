ba8908d004844e641ac2fcd262fad08d97148647
sabotenpillow <saboten.pillow@gmail.com>
2018-03-15 01:38:29 +0900

    issue close #5: specified address validation

--- opt.py
+++ opt.py
@@ -19,6 +19,7 @@ Options:
 
 from docopt import docopt
 import os
+import socket
 
 VERSION = ''.join(open('version').readline().splitlines())
 
@@ -27,10 +28,38 @@ def optparser():
     if not is_valid_ifname(args['<interface>']):
         print('invalid interface name')
         exit(-1)
+    if args['--src'] and not is_valid_ip(args['--src']):
+        print('invalid source')
+        exit(-1)
+    if args['--dst'] and not is_valid_ip(args['--dst']):
+        print('invalid destination')
+        exit(-1)
+    if args['--host'] and not is_valid_ip(args['--host']):
+        print('invalid host')
+        exit(-1)
     return args
 
 def is_valid_ifname(ifname):
     return ifname in os.listdir('/sys/class/net')
+
+def is_valid_ip(addrs):
+    for addr in addrs:
+        if not ( is_valid_ipv4(addr) or is_valid_ipv6(addr) ): return False
+    return True
+
+def is_valid_ipv4(addr):
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+    except socket.error:
+        return False
+    return True
+
+def is_valid_ipv6(addr):
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+    except socket.error:
+        return False
+    return True
 
 if __name__ == '__main__':
     print(optparser())
